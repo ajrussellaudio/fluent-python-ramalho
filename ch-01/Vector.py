bf615a930d096269a11153492df6b86766b66be6
Alan Russell <ajrussellaudio@gmail.com>
2018-03-04 20:07:11 +0000

    add addition and equality methods

--- ch-01/Vector.py
+++ ch-01/Vector.py
@@ -3,4 +3,10 @@ class Vector:
         self.x = x
         self.y = y
 
-    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
