bec126b04cced4848ba6a8434e8cb47c04bf7316
Alan Russell <ajrussellaudio@gmail.com>
2018-03-04 14:51:06 +0000

    refactor card to tuple

--- ch-01/Card.py
+++ ch-01/Card.py
@@ -1,7 +1,3 @@
-class Card:
-    def __init__(self, rank, suit):
-        self.rank = rank
-        self.suit = suit
+import collections
 
-    def __eq__(self, card):
-        return self.rank == card.rank and self.suit == card.suit
+Card = collections.namedtuple('Card', ['rank', 'suit'])
